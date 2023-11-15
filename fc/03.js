cdaaa7c793ece7ebe7aaa9300870cf067bbf497c
ejonghwan <sun87-1@daum.net>
2023-11-15 23:27:31 +0900

    동전교환 최대수열증가

--- fc/03.js
+++ fc/03.js
@@ -141,24 +141,63 @@
 
 
 
-        function solution(n, arr){
-            let answer, max=Number.MIN_SAFE_INTEGER;
-            for(let x of arr){
-                let sum=0, tmp=x;
-                while(tmp){
-                    sum+=(tmp%10);
-                    tmp=Math.floor(tmp/10);
+        // function solution(n, arr){
+        //     let answer, max=Number.MIN_SAFE_INTEGER;
+        //     for(let x of arr){
+        //         let sum=0, tmp=x;
+        //         while(tmp){
+        //             sum+=(tmp%10);
+        //             tmp=Math.floor(tmp/10);
+        //         }
+        //         if(sum>max){
+        //             max=sum;
+        //             answer=x;
+        //         }
+        //         else if(sum===max){
+        //             if(x>answer) answer=x;
+        //         }
+        //     }
+        //     return answer;
+        // }
+        
+        // let arr=[128, 460, 603, 40, 521, 137, 123];
+        // console.log(solution(7, arr));
+
+
+        // function solution(m, coin){  
+        //     let answer=0;
+        //     let dy=Array.from({length:m+1}, ()=>1000);
+        //     dy[0]=0;
+        //     for(let i=1; i<arr.length; i++){
+        //         for(let j=coin[i]; j<=m; j++){
+        //             dy[j]=Math.min(dy[j], dy[j-coin[i]]+1);
+        //         }
+        //     }
+        //     answer=dy[m];
+        //     return answer;
+        // }
+
+        // let arr=[1, 2, 5];
+        // console.log(solution(15, arr));
+
+
+        function solution(arr){  
+            let answer=0;
+            let dy=Array.from({length:arr.length}, ()=>0);
+            dy[0]=1;
+            for(let i=1; i<arr.length; i++){
+                let max=0;
+                for(let j=i-1; j>=0; j--){
+                    if(arr[j]<arr[i] && dy[j]>max){
+                        max=dy[j];
+                    }
                 }
-                if(sum>max){
-                    max=sum;
-                    answer=x;
-                }
-                else if(sum===max){
-                    if(x>answer) answer=x;
-                }
+                dy[i]=max+1;
+                answer=Math.max(answer, dy[i]);
             }
             return answer;
         }
+
+        let arr=[5, 3, 7, 8, 6, 2, 9, 4];
+        console.log(solution(arr));
         
-        let arr=[128, 460, 603, 40, 521, 137, 123];
-        console.log(solution(7, arr));
