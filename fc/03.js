cb08d6c5642b534eb31f25559c56e83a239c0122
ejonghwan <sun87-1@daum.net>
2023-11-19 17:54:15 +0900

    섬나라 문제 DFS

--- fc/03.js
+++ fc/03.js
@@ -181,23 +181,61 @@
         // console.log(solution(15, arr));
 
 
-        function solution(arr){  
+        // function solution(arr){  
+        //     let answer=0;
+        //     let dy=Array.from({length:arr.length}, ()=>0);
+        //     dy[0]=1;
+        //     for(let i=1; i<arr.length; i++){
+        //         let max=0;
+        //         for(let j=i-1; j>=0; j--){
+        //             if(arr[j]<arr[i] && dy[j]>max){
+        //                 max=dy[j];
+        //             }
+        //         }
+        //         dy[i]=max+1;
+        //         answer=Math.max(answer, dy[i]);
+        //     }
+        //     return answer;
+        // }
+
+        // let arr=[5, 3, 7, 8, 6, 2, 9, 4];
+        // console.log(solution(arr));
+        
+
+
+        function solution(board){  
             let answer=0;
-            let dy=Array.from({length:arr.length}, ()=>0);
-            dy[0]=1;
-            for(let i=1; i<arr.length; i++){
-                let max=0;
-                for(let j=i-1; j>=0; j--){
-                    if(arr[j]<arr[i] && dy[j]>max){
-                        max=dy[j];
+            let n=board.length;
+            let dx=[-1, -1, 0, 1, 1, 1, 0, -1];
+            let dy=[0, 1, 1, 1, 0, -1, -1, -1];
+            function DFS(x, y){
+               board[x][y]=0;
+               for(let k=0; k<8; k++){
+                   let nx=x+dx[k];
+                   let ny=y+dy[k];
+                   if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]===1){
+                       DFS(nx, ny);
+                   }
+               } 
+            }
+            for(let i=0; i<n; i++){
+                for(let j=0; j<n; j++){
+                    if(board[i][j]===1){
+                        board[i][j]=0;
+                        answer++;
+                        DFS(i, j);
                     }
                 }
-                dy[i]=max+1;
-                answer=Math.max(answer, dy[i]);
             }
             return answer;
         }
 
-        let arr=[5, 3, 7, 8, 6, 2, 9, 4];
+        let arr=[[1, 1, 0, 0, 0, 1, 0], 
+                 [0, 1, 1, 0, 1, 1, 0],
+                 [0, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 1, 1],
+                 [1, 1, 0, 1, 1, 0, 0],
+                 [1, 0, 0, 0, 1, 0, 0],
+                 [1, 0, 1, 0, 1, 0, 0]];
+
         console.log(solution(arr));
-        
