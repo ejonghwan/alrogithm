ad6411d6f64fd4c9fa718d58937e143a114ccb8f
ejonghwan <sun87-1@daum.net>
2023-11-25 17:01:05 +0900

    asd

--- fc/03.js
+++ fc/03.js
@@ -267,20 +267,39 @@
 
 
 
-            function solution(n){
-                let answer="";
-                function DFS(v){
-                    if(v>7) return;
-                    else{
-                        answer+=(v+' ');
-                        DFS(v*2);
-                        DFS(v*2+1);
+            // function solution(n){
+            //     let answer="";
+            //     function DFS(v){
+            //         if(v>7) return;
+            //         else{
+            //             answer+=(v+' ');
+            //             DFS(v*2);
+            //             DFS(v*2+1);
+            //         }
+            //     }
+            //     DFS(n);
+            //     return answer;
+            // }
+
+            // console.log(solution(1));
+
+        
+            function solution(s){  
+                let answer;
+                let sH = new Map();
+                for(let x of s){
+                    if(sH.has(x)) sH.set(x, sH.get(x)+1);
+                    else sH.set(x, 1);
+                }
+                let max=Number.MIN_SAFE_INTEGER;
+                for(let [key, val] of sH){
+                    if(val>max){
+                        max=val;
+                        answer=key;
                     }
                 }
-                DFS(n);
                 return answer;
             }
 
-            console.log(solution(1));
-
-        
+            let str="BACBACCACCBDEDE";
+            console.log(solution(str));
