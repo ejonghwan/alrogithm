9d4ce1cbe9e4eb0d0c9bd1fc993ac123ddb8ed3f
ejonghwan <sun87-1@daum.net>
2023-11-28 23:43:16 +0900

    세수합

--- fc/03.js
+++ fc/03.js
@@ -305,15 +305,38 @@
             // console.log(solution(str));
 
 
-            function solution(arr){
-                let answer=[];
-                let sortArr=arr.slice();
-                sortArr.sort((a, b)=>a-b);
-                for(let i=0; i<arr.length; i++){
-                    if(arr[i]!==sortArr[i]) answer.push(i+1);
+            // function solution(arr){
+            //     let answer=[];
+            //     let sortArr=arr.slice();
+            //     sortArr.sort((a, b)=>a-b);
+            //     for(let i=0; i<arr.length; i++){
+            //         if(arr[i]!==sortArr[i]) answer.push(i+1);
+            //     }
+            //     return answer;
+            // }
+
+            // let arr=[120, 125, 152, 130, 135, 135, 143, 127, 160];
+            // console.log(solution(arr));
+
+
+            function solution(n, arr){
+                let answer, max=Number.MIN_SAFE_INTEGER;
+                for(let x of arr){
+                    let sum=0, tmp=x;
+                    while(tmp){
+                        sum+=(tmp%10);
+                        tmp=Math.floor(tmp/10);
+                    }
+                    if(sum>max){
+                        max=sum;
+                        answer=x;
+                    }
+                    else if(sum===max){
+                        if(x>answer) answer=x;
+                    }
                 }
                 return answer;
             }
-
-            let arr=[120, 125, 152, 130, 135, 135, 143, 127, 160];
-            console.log(solution(arr));
+            
+            let arr=[128, 460, 603, 40, 521, 137, 123];
+            console.log(solution(7, arr));
