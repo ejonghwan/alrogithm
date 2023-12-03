1c211827cb224addfd11951cc571961eeeab8a7e
ejonghwan <sun87-1@daum.net>
2023-12-03 23:16:54 +0900

    장난꾸러기

--- fc/03.js
+++ fc/03.js
@@ -371,16 +371,30 @@
             // console.log(solution(5, 14));
 
 
-            function solution(k, arr){
-                let answer, sum=0;
-                for(let i=0; i<k; i++) sum+=arr[i];
-                answer=sum;
-                for(let i=k; i<arr.length; i++){
-                    sum+=(arr[i]-arr[i-k]);
-                    answer=Math.max(answer, sum);
-                }                    
+            // function solution(k, arr){
+            //     let answer, sum=0;
+            //     for(let i=0; i<k; i++) sum+=arr[i];
+            //     answer=sum;
+            //     for(let i=k; i<arr.length; i++){
+            //         sum+=(arr[i]-arr[i-k]);
+            //         answer=Math.max(answer, sum);
+            //     }                    
+            //     return answer;
+            // }
+            
+            // let a=[12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+            // console.log(solution(3, a));
+
+            // /장난꾸러기
+            function solution(arr){
+                let answer=[];
+                let sortArr=arr.slice();
+                sortArr.sort((a, b)=>a-b);
+                for(let i=0; i<arr.length; i++){
+                    if(arr[i]!==sortArr[i]) answer.push(i+1);
+                }
                 return answer;
             }
-            
-            let a=[12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
-            console.log(solution(3, a));
+
+            let arr=[120, 125, 152, 130, 135, 135, 143, 127, 160];
+            console.log(solution(arr));
