38d294577371e842be017d24fcf110ee6dbcb392
ejonghwan <sun87-1@daum.net>
2023-12-14 22:32:09 +0900

    최대 매출

--- fc/03.js
+++ fc/03.js
@@ -463,18 +463,33 @@
             // console.log(solution(8, 3));
             
 
-            function solution(str){
-                let answer="";
-                for(let x of str){
-                    if(!isNaN(x)) answer+=x;
-                }  
-                return parseInt(answer);
+            // function solution(str){
+            //     let answer="";
+            //     for(let x of str){
+            //         if(!isNaN(x)) answer+=x;
+            //     }  
+            //     return parseInt(answer);
+            // }
+            
+            // let str="g0en2T0s8eSoft";
+            // console.log(solution(str));
+
+
+
+            // 최대매출
+            function solution(k, arr){
+                let answer, sum=0;
+                for(let i=0; i<k; i++) sum+=arr[i];
+                answer=sum;
+                for(let i=k; i<arr.length; i++){
+                    sum+=(arr[i]-arr[i-k]);
+                    answer=Math.max(answer, sum);
+                }                    
+                return answer;
             }
             
-            let str="g0en2T0s8eSoft";
-            console.log(solution(str));
-
-
+            let a=[12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+            console.log(solution(3, a));
 
 
             
