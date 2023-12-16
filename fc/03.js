70c9f61e2b0af46b9bcfc0942b36d42dd01c6f83
ejonghwan <sun87-1@daum.net>
2023-12-16 20:18:19 +0900

    졸업선물

--- fc/03.js
+++ fc/03.js
@@ -492,22 +492,44 @@
             // console.log(solution(3, a));
 
 
-            // 공주구하기
-            function solution(s){  
-                let answer;
-                let stack=[];
-                for(let x of s){
-                    if(x===')'){
-                        while(stack.pop()!=='(');
+            // // 공주구하기
+            // function solution(s){  
+            //     let answer;
+            //     let stack=[];
+            //     for(let x of s){
+            //         if(x===')'){
+            //             while(stack.pop()!=='(');
+            //         }
+            //         else stack.push(x);
+            //     }
+            //     answer=stack.join('');
+            //     return answer;
+            // }
+
+            // let str="(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+            // console.log(solution(str));
+
+            
+            // 졸업선물
+            function solution(m, product){
+                let answer=0;
+                let n=product.length;
+                product.sort((a, b)=>(a[0]+a[1])-(b[0]+b[1]));
+                for(let i=0; i<n; i++){
+                    let money=m-(product[i][0]/2+product[i][1]);
+                    let cnt=1;
+                    for(let j=0; j<n; j++){
+                        if(j!==i && (product[j][0]+product[j][1])>money) break;
+                        if(j!==i && (product[j][0]+product[j][1])<=money){
+                            money-=(product[j][0]+product[j][1]);
+                            cnt++;
+                        }
                     }
-                    else stack.push(x);
-                }
-                answer=stack.join('');
+                    answer=Math.max(answer, cnt);
+                }  
                 return answer;
             }
-
-            let str="(A(BC)D)EF(G(H)(IJ)K)LM(N)";
-            console.log(solution(str));
-
-
+            
+            let arr=[[6, 6], [2, 2], [4, 3], [4, 5], [10, 3]];
+            console.log(solution(28, arr));
             
