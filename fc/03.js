9acf6be49f0b1943e38d54fe2615edbaf401ede2
ejonghwan <sun87-1@daum.net>
2023-12-24 23:44:05 +0900

    멘토링

--- fc/03.js
+++ fc/03.js
@@ -626,16 +626,41 @@
             // let arr=[130, 135, 148, 140, 145, 150, 150, 153];
             // console.log(solution(arr));
 
-            function solution(arr){         
-                let answer=1, max=arr[0];
-                for(let i=1; i<arr.length; i++){
-                    if(arr[i]>max){
-                        answer++;
-                        max=arr[i];
+            // function solution(arr){         
+            //     let answer=1, max=arr[0];
+            //     for(let i=1; i<arr.length; i++){
+            //         if(arr[i]>max){
+            //             answer++;
+            //             max=arr[i];
+            //         }
+            //     }
+            //     return answer;
+            // }
+
+            // let arr=[130, 135, 148, 140, 145, 150, 150, 153];
+            // console.log(solution(arr));
+
+        // 멘토링
+            function solution(test){
+                let answer=0;
+                m=test.length;
+                n=test[0].length;
+                for(let i=1; i<=n; i++){
+                    for(let j=1; j<=n; j++){
+                        let cnt=0;
+                        for(let k=0; k<m; k++){
+                            let pi=pj=0;
+                            for(let s=0; s<n; s++){
+                                if(test[k][s]===i) pi=s;
+                                if(test[k][s]===j) pj=s;
+                            }
+                            if(pi<pj) cnt++;
+                        }
+                        if(cnt===m) answer++;
                     }
                 }
                 return answer;
             }
-
-            let arr=[130, 135, 148, 140, 145, 150, 150, 153];
+            
+            let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
             console.log(solution(arr));
