b448eefcdfd86cd9b809ab848e59ba4c69e86f77
ejonghwan <sun87-1@daum.net>
2023-12-26 23:43:54 +0900

    합이 같은 부분 집합

--- fc/03.js
+++ fc/03.js
@@ -664,3 +664,29 @@
             
             // let arr=[[3, 4, 1, 2], [4, 3, 2, 1], [3, 1, 4, 2]];
             // console.log(solution(arr));
+
+
+            // 합이 같은 부분 집함
+            function solution(arr){
+                let answer="NO", flag=0;
+                let total=arr.reduce((a, b)=>a+b, 0);
+                let n=arr.length;
+                function DFS(L, sum){
+                    if(flag) return;
+                    if(L===n){
+                        if((total-sum)===sum){
+                            answer="YES";
+                            flag=1;
+                        }
+                    }
+                    else{
+                        DFS(L+1, sum+arr[L]);
+                        DFS(L+1, sum);
+                    }
+                }
+                DFS(0, 0);
+                return answer;
+            }
+
+            let arr=[1, 3, 5, 6, 7, 10];
+            console.log(solution(arr));
