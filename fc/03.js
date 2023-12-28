f10cd2f444e2835fb2169d78ec089e3647ad04af
ejonghwan <sun87-1@daum.net>
2023-12-28 23:54:24 +0900

    등수구하기

--- fc/03.js
+++ fc/03.js
@@ -693,24 +693,40 @@
 
 
             // 졸업선물
-            function solution(m, product){
-                let answer=0;
-                let n=product.length;
-                product.sort((a, b)=>(a[0]+a[1])-(b[0]+b[1]));
+            // function solution(m, product){
+            //     let answer=0;
+            //     let n=product.length;
+            //     product.sort((a, b)=>(a[0]+a[1])-(b[0]+b[1]));
+            //     for(let i=0; i<n; i++){
+            //         let money=m-(product[i][0]/2+product[i][1]);
+            //         let cnt=1;
+            //         for(let j=0; j<n; j++){
+            //             if(j!==i && (product[j][0]+product[j][1])>money) break;
+            //             if(j!==i && (product[j][0]+product[j][1])<=money){
+            //                 money-=(product[j][0]+product[j][1]);
+            //                 cnt++;
+            //             }
+            //         }
+            //         answer=Math.max(answer, cnt);
+            //     }  
+            //     return answer;
+            // }
+            
+            // let arr=[[6, 6], [2, 2], [4, 3], [4, 5], [10, 3]];
+            // console.log(solution(28, arr));
+
+
+            // 등수 구하기 
+            function solution(arr){  
+                let n=arr.length;
+                let answer=Array.from({length:n}, ()=>1);
                 for(let i=0; i<n; i++){
-                    let money=m-(product[i][0]/2+product[i][1]);
-                    let cnt=1;
                     for(let j=0; j<n; j++){
-                        if(j!==i && (product[j][0]+product[j][1])>money) break;
-                        if(j!==i && (product[j][0]+product[j][1])<=money){
-                            money-=(product[j][0]+product[j][1]);
-                            cnt++;
-                        }
+                        if(arr[j]>arr[i]) answer[i]++;
                     }
-                    answer=Math.max(answer, cnt);
-                }  
+                }             
                 return answer;
             }
-            
-            let arr=[[6, 6], [2, 2], [4, 3], [4, 5], [10, 3]];
-            console.log(solution(28, arr));
+
+            let arr=[87, 89, 92, 100, 76];
+            console.log(solution(arr));
