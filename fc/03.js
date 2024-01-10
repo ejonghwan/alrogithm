5b7dd82df40e1197f8ea2d1e4e07d8ea0fff21b5
ejonghwan <sun87-1@daum.net>
2024-01-10 23:55:41 +0900

    케이번쨰

--- fc/03.js
+++ fc/03.js
@@ -858,30 +858,51 @@
             // console.log(solution(arr));
 
 
-            function solution(arr){  
-                let answer=Number.MIN_SAFE_INTEGER;
-                let n=arr.length;
-                let sum1=sum2=0;
+            // function solution(arr){  
+            //     let answer=Number.MIN_SAFE_INTEGER;
+            //     let n=arr.length;
+            //     let sum1=sum2=0;
+            //     for(let i=0; i<n; i++){
+            //         sum1=sum2=0;
+            //         for(let j=0; j<n; j++){
+            //             sum1+=arr[i][j];
+            //             sum2+=arr[j][i];
+            //         }
+            //         answer=Math.max(answer, sum1, sum2);
+            //     }
+            //     sum1=sum2=0;
+            //     for(let i=0; i<n; i++){
+            //         sum1+=arr[i][i];
+            //         sum2+=arr[i][n-i-1];
+            //     }  
+            //     answer=Math.max(answer, sum1, sum2); 
+            //     return answer;
+            // }
+
+            // let arr=[[10, 13, 10, 12, 15], 
+            //          [12, 39, 30, 23, 11],
+            //          [11, 25, 50, 53, 15],
+            //          [19, 27, 29, 37, 27],
+            //          [19, 13, 30, 13, 19]];
+            // console.log(solution(arr));
+
+
+            function solution(n, k, card){
+                let answer;
+                let tmp = new Set();
                 for(let i=0; i<n; i++){
-                    sum1=sum2=0;
-                    for(let j=0; j<n; j++){
-                        sum1+=arr[i][j];
-                        sum2+=arr[j][i];
+                    for(let j=i+1; j<n; j++){
+                        for(let k=j+1; k<n; k++){
+                            tmp.add(card[i]+card[j]+card[k]);
+                        }
                     }
-                    answer=Math.max(answer, sum1, sum2);
                 }
-                sum1=sum2=0;
-                for(let i=0; i<n; i++){
-                    sum1+=arr[i][i];
-                    sum2+=arr[i][n-i-1];
-                }  
-                answer=Math.max(answer, sum1, sum2); 
+                let a=Array.from(tmp).sort((a, b)=>b-a);
+                answer=a[k-1];
                 return answer;
             }
+            
+            let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+            console.log(solution(10, 3, arr));
+            
 
-            let arr=[[10, 13, 10, 12, 15], 
-                     [12, 39, 30, 23, 11],
-                     [11, 25, 50, 53, 15],
-                     [19, 27, 29, 37, 27],
-                     [19, 13, 30, 13, 19]];
-            console.log(solution(arr));
