320bb1036edcf9e6ee1b79e6cb676f2c865afaa4
ejonghwan <sun87-1@daum.net>
2024-01-12 23:42:25 +0900

    ...

--- fc/03.js
+++ fc/03.js
@@ -905,33 +905,33 @@
             // let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
             // console.log(solution(10, 3, arr));
             
-            // 후위식 인형뽑기
-            function solution(board, moves){
-                let answer=0;
-                let stack=[];
-                moves.forEach(pos => {
-                    for(let i=0; i<board.length; i++){
-                        if(board[i][pos-1]!==0){
-                            let tmp=board[i][pos-1];
-                            board[i][pos-1]=0;
-                            if(tmp===stack[stack.length-1]){
-                                stack.pop();
-                                answer+=2;
-                            }
-                            else stack.push(tmp);
-                            break;
-                        }
-                    }
-                });
+            // // 후위식 인형뽑기
+            // function solution(board, moves){
+            //     let answer=0;
+            //     let stack=[];
+            //     moves.forEach(pos => {
+            //         for(let i=0; i<board.length; i++){
+            //             if(board[i][pos-1]!==0){
+            //                 let tmp=board[i][pos-1];
+            //                 board[i][pos-1]=0;
+            //                 if(tmp===stack[stack.length-1]){
+            //                     stack.pop();
+            //                     answer+=2;
+            //                 }
+            //                 else stack.push(tmp);
+            //                 break;
+            //             }
+            //         }
+            //     });
                                 
-                return answer;
-            }
+            //     return answer;
+            // }
             
-            let a=[[0,0,0,0,0],
-                   [0,0,1,0,3],
-                   [0,2,5,0,1],
-                   [4,2,4,4,2],
-                   [3,5,1,3,1]];
+            // let a=[[0,0,0,0,0],
+            //        [0,0,1,0,3],
+            //        [0,2,5,0,1],
+            //        [4,2,4,4,2],
+            //        [3,5,1,3,1]];
 
-            let b=[1, 5, 3, 5, 1, 2, 1, 4];
-            console.log(solution(a, b));
+            // let b=[1, 5, 3, 5, 1, 2, 1, 4];
+            // console.log(solution(a, b));
