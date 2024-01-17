5ef2b9c1395f6ff158b8a3a798a1e6f024fb5e70
ejonghwan <sun87-1@daum.net>
2024-01-17 23:45:46 +0900

    ...

--- fc/03.js
+++ fc/03.js
@@ -974,17 +974,17 @@
 
 
             // 연속 부분 수열 
-            function solution(m, arr){
-                let answer=0, sum=0, lt=0;
-                for(let rt=0; rt<arr.length; rt++){
-                    sum+=arr[rt];
-                    while(sum>m){
-                        sum-=arr[lt++];
-                    }
-                    answer+=(rt-lt+1);
-                }               
-                return answer;
-            }
+            // function solution(m, arr){
+            //     let answer=0, sum=0, lt=0;
+            //     for(let rt=0; rt<arr.length; rt++){
+            //         sum+=arr[rt];
+            //         while(sum>m){
+            //             sum-=arr[lt++];
+            //         }
+            //         answer+=(rt-lt+1);
+            //     }               
+            //     return answer;
+            // }
             
-            let a=[1, 3, 1, 2, 3];
-            console.log(solution(5, a));
+            // let a=[1, 3, 1, 2, 3];
+            // console.log(solution(5, a));
