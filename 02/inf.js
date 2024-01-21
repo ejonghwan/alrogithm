8d684d329470505fa654b9ea93bfe20dab3fc0c1
ejonghwan <sun87-1@daum.net>
2024-01-21 23:22:41 +0900

    인프런 알고리즘 시작

---
+++ 02/inf.js
@@ -0,0 +1,37 @@
+
+
+const log = console.log;
+
+// const a = (a, b, c) => {
+//     let answer;
+//     return answer
+// }
+// log( a() )
+
+
+
+
+
+
+// 최솟값 구하기
+// const a = (a, b, c) => {
+//     let answer;
+//     if(a < b) answer = a;
+//     else answer = b;
+//     if(c < answer) answer = c; 
+//     return answer;
+// }
+// log( a(3,2, 11) )
+
+
+
+
+// 삼각형 유무 
+// 큰 막대의 길이가 짧은 막대 2개의 합보다 작아야함
+
+const a = (a, b, c) => {
+    let answer;
+    return answer
+}
+log( a() )
+
