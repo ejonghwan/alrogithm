d5393e3dd8445ca25325dd3d016b0e2be28697c4
ejonghwan <sun87-1@daum.net>
2024-01-22 00:27:23 +0900

    삼각형 유무

--- 02/inf.js
+++ 02/inf.js
@@ -27,11 +27,22 @@ const log = console.log;
 
 
 // 삼각형 유무 
-// 큰 막대의 길이가 짧은 막대 2개의 합보다 작아야함
+// 삼각형이 되려면 큰 막대의 길이가 짧은 막대 2개의 합보다 작아야함
 
 const a = (a, b, c) => {
-    let answer;
+    let answer = 'yes', max;
+    let sum = a + b + c; // 세 길이의 총합
+
+    if(a > b) max = a;
+    else max = b;
+    if(c > max) max = c;
+    //맥스에 가장 큰수 넣기
+
+    // 총 길이에서 맥스를 뻄
+    if((sum - max) <= max) answer = 'no';
+    
+
     return answer
 }
-log( a() )
+log( a(6, 7, 11) )
 
